5365f333eab20f4a0936ce3d641c7c428e0623f7
Ali Selcuk <maselcuk@gmail.com>
2017-09-06 03:42:21 +0300

    timeout

--- src/Features/InstallNginx.php
+++ src/Features/InstallNginx.php
@@ -14,51 +14,51 @@ class InstallNginx extends AgentFeature
 {
     public function handle(StubBuilder $builder)
     {
-//        $distro = 'jessie';
-//
-//        $this->job('Add DotDeb Sources',
-//            $builder->wrapper('save_to_file', ['target' => '/etc/apt/sources.list.d/dotdeb.list'])
-//                    ->build('superv.agents.nginx::dotdeb_sources', ['distro' => $distro])
-//        );
-//
-//        $this->job('Install GnuPG Key', 'wget -q -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -');
-//
-//        $this->job('Update Apt', 'apt-get update');
-//
-//        $this->job('Install Nginx', 'apt-get install -y nginx');
-//        $this->job('Install PHP 7.0', 'apt-get install -y php7.0-fpm');
-//
-//        $this->job('Install PHP Extensions',
-//            'apt-get install -y php7.0-memcached php7.0-mysql php7.0-gd php7.0-mcrypt php7.0-curl php7.0-sqlite3 php7.0-mbstring php7.0-xml php7.0-zip'
-//        );
-//
-//        $this->job('Install Git', 'apt-get install -y git');
-//        $this->job('Install Composer', $this->dispatch(new MakeStub('superv::installer.composer')));
-//
-//        $this->server->mkdirR('/etc/supervisor/conf.d');
-//        $this->server->saveToFile(
-//            $this->dispatch(new MakeStub('superv::installer.supervisor', ['home_dir' => '/usr/local/superv/app'])),
-//            '/etc/supervisor/conf.d/superv.conf'
-//        );
-//
-//        $this->job('Install Redis Server', 'apt-get install -y redis-server');
-//        $this->job('Start Redis Server', 'service redis-server start');
-//
-//        $this->job('Get NodeJS Source', 'wget -q -O - https://deb.nodesource.com/setup_8.x | bash -');
-//        $this->job('Install NodeJS', 'apt-get install -y nodejs');
-//        $this->job('Upgrade NPM', 'npm install npm@latest -g');
-//
-//        $mysqlAdminPass = base64_encode(random_bytes(16));
-//        $this->server->saveToFile("[client]\nuser=root\npassword={$mysqlAdminPass}", '/root/.my.cnf');
-//
-//        $this->job('Install Mysql Server')
-//             ->stub('superv.agents.power_dns::install_mysql', ['mysql_admin_pass' => $mysqlAdminPass]);
+        $distro = 'jessie';
 
-//        $mysqlAppPass = base64_encode(random_bytes(16));
-        $mysqlAppPass = 'sallama';
-//        $this->job('Create SuperV DB', "mysql -u root -e \"CREATE DATABASE superv_app;\"");
-//        $this->job('Create SuperV DB User', "mysql -u root -e \"CREATE USER 'superv_app'@'localhost' IDENTIFIED BY '{$mysqlAppPass}';\"");
-//        $this->job('Grant DB PRIVILEGES', "mysql -u root -e \"GRANT ALL ON superv_app.* TO 'superv_app'@'localhost'; FLUSH PRIVILEGES;\"");
+        $this->job('Add DotDeb Sources',
+            $builder->wrapper('save_to_file', ['target' => '/etc/apt/sources.list.d/dotdeb.list'])
+                    ->build('superv.agents.nginx::dotdeb_sources', ['distro' => $distro])
+        );
+
+        $this->job('Install GnuPG Key', 'wget -q -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -');
+
+        $this->job('Update Apt', 'apt-get update');
+
+        $this->job('Install Nginx', 'apt-get install -y nginx');
+        $this->job('Install PHP 7.0', 'apt-get install -y php7.0-fpm');
+
+        $this->job('Install PHP Extensions',
+            'apt-get install -y php7.0-memcached php7.0-mysql php7.0-gd php7.0-mcrypt php7.0-curl php7.0-sqlite3 php7.0-mbstring php7.0-xml php7.0-zip'
+        );
+
+        $this->job('Install Git', 'apt-get install -y git');
+        $this->job('Install Composer', $this->dispatch(new MakeStub('superv::installer.composer')));
+
+        $this->server->mkdirR('/etc/supervisor/conf.d');
+        $this->server->saveToFile(
+            $this->dispatch(new MakeStub('superv::installer.supervisor', ['home_dir' => '/usr/local/superv/app'])),
+            '/etc/supervisor/conf.d/superv.conf'
+        );
+
+        $this->job('Install Redis Server', 'apt-get install -y redis-server');
+        $this->job('Start Redis Server', 'service redis-server start');
+
+        $this->job('Get NodeJS Source', 'wget -q -O - https://deb.nodesource.com/setup_8.x | bash -');
+        $this->job('Install NodeJS', 'apt-get install -y nodejs');
+        $this->job('Upgrade NPM', 'npm install npm@latest -g');
+
+        $mysqlAdminPass = base64_encode(random_bytes(16));
+        $this->server->saveToFile("[client]\nuser=root\npassword={$mysqlAdminPass}", '/root/.my.cnf');
+
+        $this->job('Install Mysql Server')
+             ->stub('superv.agents.power_dns::install_mysql', ['mysql_admin_pass' => $mysqlAdminPass]);
+
+        $mysqlAppPass = base64_encode(random_bytes(16));
+//        $mysqlAppPass = 'sallama';
+        $this->job('Create SuperV DB', "mysql -u root -e \"CREATE DATABASE superv_app;\"");
+        $this->job('Create SuperV DB User', "mysql -u root -e \"CREATE USER 'superv_app'@'localhost' IDENTIFIED BY '{$mysqlAppPass}';\"");
+        $this->job('Grant DB PRIVILEGES', "mysql -u root -e \"GRANT ALL ON superv_app.* TO 'superv_app'@'localhost'; FLUSH PRIVILEGES;\"");
 
         $this->server->mkdirR('/usr/local/superv');
 //        $this->job('Composer Github Token', 'composer config -g github-oauth.github.com dfd37cbc260cbb29f310f329f0a37f21dcf32328');
